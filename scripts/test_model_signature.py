2db02f22cd027e14ed910272ea0ace2aad56a11f
abubakarsaddique22 <abubakarsaddique3434@gmail.com>
2025-09-18 09:44:43 +0500

    added ci/cd

--- scripts/test_model_signature.py
+++ scripts/test_model_signature.py
@@ -8,7 +8,7 @@ from mlflow.tracking import MlflowClient
 mlflow.set_tracking_uri('http://13.61.25.27:5000/')
 
 @pytest.mark.parametrize("model_name, stage, vectorizer_path", [
-    ("yt_chrome_plugin_model", "staging", "tfidf_vectorizer.pkl"),  # Replace with your actual model name and vectorizer path
+    ("yt_chrome_plugin_model", "staging", "models/vectorizer.pkl"),  # Replace with your actual model name and vectorizer path
 ])
 def test_model_with_vectorizer(model_name, stage, vectorizer_path):
     client = MlflowClient()
