10f0acc19f9b9add74b6a14ef675042984f9b60a
abubakarsaddique22 <abubakarsaddique3434@gmail.com>
2025-09-18 10:00:32 +0500

    some changeing

--- scripts/test_model_signature.py
+++ scripts/test_model_signature.py
@@ -46,6 +46,3 @@ def test_model_with_vectorizer(model_name, stage, vectorizer_path):
 
     except Exception as e:
         pytest.fail(f"Model test failed with error: {e}")
-
-
-print('some text added for push up ')
